fed65d84f3462bbfb9aa96499d36ccde9149869a
jainpradeep <jainpradeep.iitm@gmail.com>
2018-10-29 16:32:16 +0530

    Removing static sub menu items

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -19,47 +19,47 @@
   /** @ngInject */
   function routeConfig($urlRouterProvider, baSidebarServiceProvider) {
     $urlRouterProvider.otherwise('/authSignIn');
-    baSidebarServiceProvider.addStaticItem({
-      title: 'Bijwasan',
-      icon: 'ion-ios-more',
-      subMenu: [{
-        title: 'Delhi (EX-MR)',
-        disabled: true
-      },{
-        title: 'SKO LBT-Pumping',
-        disabled: true
-      },{
-        title: 'Delhi (EX-PR)',
-        disabled: true
-      }, {
-        title: 'Daily Report',
-        subMenu: [{
-          title: 'Rev-pumping Pnp-Del',
-          disabled: true
-        },{
-          title: 'Delivery Del',
-          disabled: true
-        },{
-          title: 'Rev-Delivery Del',
-          disabled: true
-        },{
-          title: 'Pumping Del-Pnp',
-          disabled: true
-        },{
-          title: 'Deliery Pnp (PDPL 14")',
-          disabled: true
-        }]
-      },{
-        title: 'Equipment running hours of Bijwasan',
-        disabled: true
-      },{
-        title: 'Product in Station Linefill',
-        disabled: true
-      },{
-        title: 'Product Planning',
-        disabled: true
-      }]
-    });
+    // baSidebarServiceProvider.addStaticItem({
+    //   title: 'Bijwasan',
+    //   icon: 'ion-ios-more',
+    //   subMenu: [{
+    //     title: 'Delhi (EX-MR)',
+    //     disabled: true
+    //   },{
+    //     title: 'SKO LBT-Pumping',
+    //     disabled: true
+    //   },{
+    //     title: 'Delhi (EX-PR)',
+    //     disabled: true
+    //   }, {
+    //     title: 'Daily Report',
+    //     subMenu: [{
+    //       title: 'Rev-pumping Pnp-Del',
+    //       disabled: true
+    //     },{
+    //       title: 'Delivery Del',
+    //       disabled: true
+    //     },{
+    //       title: 'Rev-Delivery Del',
+    //       disabled: true
+    //     },{
+    //       title: 'Pumping Del-Pnp',
+    //       disabled: true
+    //     },{
+    //       title: 'Deliery Pnp (PDPL 14")',
+    //       disabled: true
+    //     }]
+    //   },{
+    //     title: 'Equipment running hours of Bijwasan',
+    //     disabled: true
+    //   },{
+    //     title: 'Product in Station Linefill',
+    //     disabled: true
+    //   },{
+    //     title: 'Product Planning',
+    //     disabled: true
+    //   }]
+    // });
   }
 
 })();
